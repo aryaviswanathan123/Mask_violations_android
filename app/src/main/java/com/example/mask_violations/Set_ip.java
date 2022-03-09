1a1af7e672447d7e0b103eb782675aee93b56e87
aryaviswanathan123 <74293440+aryaviswanathan123@users.noreply.github.com>
2022-03-09 10:37:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mask_violations/Set_ip.java
@@ -0,0 +1,13 @@
+package com.example.mask_violations;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Set_ip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_ip);
+    }
+}
